7aa303dddc0922b2e1ca92dcbb4860bccbadc143
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-04 12:27:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total = @total + (price * quantity)
-    @items << title 
+    @items << price  
   end
 
   def apply_discount
