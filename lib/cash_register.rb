b92c318a6d7738bf7c3673bdc921a4f0b21492a1
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-04 12:12:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,16 @@
 class CashRegister
 
-  attr_accessor :total, :discount 
+  attr_accessor :total, :discount
 
   def initialize(discount = 0)
     @total = 0
   end
 
-  def add_item
+  def add_item(title, price) 
   end
 
   def discount
-    @total 
+    @total
   end
 
 end
